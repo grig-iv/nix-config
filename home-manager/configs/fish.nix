607dc0855108b01fa7aee4eed3bf37a04e11b4fe
grig-iv <abstractgrig@gmail.com>
2024-02-01 23:58:47 +0300

    fix fish theme + add clear command

--- home-manager/configs/fish.nix
+++ home-manager/configs/fish.nix
@@ -87,34 +87,17 @@ in {
 
     shellInit = ''
       bind \cq 'exit'
-
-      set --universal fish_color_normal cdd6f4
-      set --universal fish_color_command 89b4fa
-      set --universal fish_color_param f2cdcd
-      set --universal fish_color_keyword f38ba8
-      set --universal fish_color_quote a6e3a1
-      set --universal fish_color_redirection f5c2e7
-      set --universal fish_color_end fab387
-      set --universal fish_color_comment 7f849c
-      set --universal fish_color_error f38ba8
-      set --universal fish_color_gray 6c7086
-      set --universal fish_color_selection --background=313244
-      set --universal fish_color_search_match --background=313244
-      set --universal fish_color_option a6e3a1
-      set --universal fish_color_operator f5c2e7
-      set --universal fish_color_escape eba0ac
-      set --universal fish_color_autosuggestion 6c7086
-      set --universal fish_color_cancel f38ba8
-      set --universal fish_color_cwd f9e2af
-      set --universal fish_color_user 94e2d5
-      set --universal fish_color_host 89b4fa
-      set --universal fish_color_host_remote a6e3a1
-      set --universal fish_color_status f38ba8
-      set --universal fish_pager_color_progress 6c7086
-      set --universal fish_pager_color_prefix f5c2e7
-      set --universal fish_pager_color_completion cdd6f4
-      set --universal fish_pager_color_description 6c7086
+      bind \cl 'clear; commandline -f repaint'
     '';
+
+    interactiveShellInit = ''
+      fish_config theme save "Catppuccin Mocha"
+    '';
+  };
+
+  xdg.configFile."fish/themes/Catppuccin Mocha.theme".source = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/fish/main/themes/Catppuccin%20Mocha.theme";
+    sha256 = "MlI9Bg4z6uGWnuKQcZoSxPEsat9vfi5O1NkeYFaEb2I=";
   };
 
   programs.zoxide.enable = true;
