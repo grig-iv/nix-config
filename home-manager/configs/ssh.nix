189366d18bedf4455e97ac704979187a520786f8
grig-iv <abstractgrig@gmail.com>
2024-02-12 00:31:22 +0300

    add golden-ratio vps

--- home-manager/configs/ssh.nix
+++ home-manager/configs/ssh.nix
@@ -4,6 +4,7 @@
   sops.secrets = {
     "ssh/vps-nl".path = ".ssh/vps-nl";
     "ssh/vps-no".path = ".ssh/vps-no";
+    "ssh/vps-golden-ratio".path = ".ssh/vps-golden-ratio";
     "ssh/hf".path = ".ssh/hf";
   };
 
@@ -23,6 +24,11 @@
         host = "38.180.77.225";
         identityFile = "~/.ssh/vps-no";
         hostname = "38.180.77.225";
+      };
+      vps-golden-ratio = {
+        host = "5.8.51.147";
+        identityFile = "~/.ssh/vps-golden-ratio";
+        hostname = "5.8.51.147";
       };
     };
   };
