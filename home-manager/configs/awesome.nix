1871548f26b3800f2fac69e2e849fe543fa768dd
grig-iv <abstractgrig@gmail.com>
2024-03-22 23:29:10 +0300

    add sxhkd (doesn't work yet)

--- home-manager/configs/awesome.nix
+++ home-manager/configs/awesome.nix
@@ -1,9 +1,20 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   xsession.windowManager.awesome.enable = true;
 
   my.shell.bookmarks.a = "~/.config/awesome";
 
-  services.sxhkd.enable = true;
+  services.sxhkd = {
+    enable = true;
+    # TODO: check why don't work
+    keybindings = {
+      "XF86Audio{Lower,Raise}Volume" = "${lib.getExe pkgs.pamixer} -{d,i} 5";
+      "XF86AudioMute" = "${lib.getExe pkgs.pamixer} -t";
+    };
+  };
 
   home = {
     packages = [pkgs.sxhkd];
