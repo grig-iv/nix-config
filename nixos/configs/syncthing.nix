7380a11d03af7dc26c79a108caf7a4061771cc0f
grig-iv <abstractgrig@gmail.com>
2024-04-27 00:37:29 +0300

    syncthing: add hard link to freetube

--- nixos/configs/syncthing.nix
+++ nixos/configs/syncthing.nix
@@ -5,70 +5,84 @@
 }: let
   host = config.my.host;
   user = config.my.user;
-in {
-  imports = [./sops.nix];
+  homePath = "/home/${user}";
+  cloudPath = "${homePath}/Cloud";
+in
+  with lib; {
+    imports = [./sops.nix];
 
-  sops.secrets."syncthing/key-${host}" = {};
-  sops.secrets."syncthing/cert-${host}" = {};
-  services.syncthing = {
-    enable = true;
-    user = user;
-    group = "wheel";
-    openDefaultPorts = lib.mkDefault true;
-    key = config.sops.secrets."syncthing/key-${host}".path;
-    cert = config.sops.secrets."syncthing/cert-${host}".path;
-    configDir = "/home/${user}/.config/syncthing";
-    extraFlags = ["--no-default-folder"];
-    settings = {
-      gui.theme = "black";
-      options = {
-        globalAnnounceEnabled = true;
-        urAccepted = -1;
-      };
-      devices = {
-        "phone".id = "WQ2IBG5-LHTOBE5-QAKLUKH-NXKZZZX-SMIMQ7T-2HLUXTM-FNVSH4Z-Y3QTVAD";
-        "work-wsl".id = "XWUW5YU-B2MTUK7-33TJ4TM-ZKNMUT3-RAQKWBT-SXBDDUF-4I4EGIZ-OBIXPQP";
-        "tha-wsl".id = "SOQ6HXT-ZGVK6BQ-FOOWUEI-TIH4CQT-4A6ALQV-FKWNRX5-4IAT5XS-7S3GRQO";
-        "tha-mili".id = "HV35JBN-LZLIZWK-KSZTMLD-3J25IYS-DYDYIAN-B3A7LNE-EQIPAJH-AQMVPQR";
-        "xtal".id = "Q5QZIKG-DLRF2VN-6PC6TEQ-VT324HL-2DLDYLC-OWPXUMD-4KRLX5X-L76DKQH";
-      };
-      folders = lib.filterAttrs (n: v: lib.any (d: d == host) v.devices) {
-        "Extended Mind" = {
-          id = "tcgvs-gd77k";
-          path = "/home/${user}/Extended Mind";
-          devices = ["phone" "xtal" "tha-wsl" "work-wsl"];
+    sops.secrets."syncthing/key-${host}" = {};
+    sops.secrets."syncthing/cert-${host}" = {};
+    services.syncthing = {
+      enable = true;
+      user = user;
+      group = "wheel";
+      openDefaultPorts = mkDefault true;
+      key = config.sops.secrets."syncthing/key-${host}".path;
+      cert = config.sops.secrets."syncthing/cert-${host}".path;
+      configDir = "/home/${user}/.config/syncthing";
+      extraFlags = ["--no-default-folder"];
+      settings = {
+        gui.theme = "black";
+        options = {
+          globalAnnounceEnabled = true;
+          urAccepted = -1;
         };
-        "Quick Share" = {
-          id = "mudfh-6fzjh";
-          path = "/home/${user}/Quick Share";
-          devices = ["phone" "xtal" "tha-wsl" "work-wsl"];
+        devices = {
+          "phone".id = "WQ2IBG5-LHTOBE5-QAKLUKH-NXKZZZX-SMIMQ7T-2HLUXTM-FNVSH4Z-Y3QTVAD";
+          "work-wsl".id = "XWUW5YU-B2MTUK7-33TJ4TM-ZKNMUT3-RAQKWBT-SXBDDUF-4I4EGIZ-OBIXPQP";
+          "tha-wsl".id = "SOQ6HXT-ZGVK6BQ-FOOWUEI-TIH4CQT-4A6ALQV-FKWNRX5-4IAT5XS-7S3GRQO";
+          "tha-mili".id = "HV35JBN-LZLIZWK-KSZTMLD-3J25IYS-DYDYIAN-B3A7LNE-EQIPAJH-AQMVPQR";
+          "xtal".id = "Q5QZIKG-DLRF2VN-6PC6TEQ-VT324HL-2DLDYLC-OWPXUMD-4KRLX5X-L76DKQH";
         };
-        ".config-win" = {
-          id = "wutmt-nb32y";
-          path = "/mnt/c/Users/" + config.my.windows.user + "/.config";
-          devices = ["phone" "tha-wsl" "work-wsl"];
-        };
-        "Books" = {
-          id = "ztshj-sh3ra";
-          path = "/home/${user}/Books";
-          devices = ["phone" "xtal" "tha-wsl"];
-        };
-        "Camera" = {
-          id = "7pgoy-oxxrb";
-          path = "/home/${user}/Camera";
-          devices = ["phone" "xtal" "tha-wsl"];
-        };
-        "Interlinked Cells" = {
-          id = "rfkva-mxs67";
-          path = "/home/${user}/Interlinked Cells";
-          devices = ["phone" "xtal" "tha-wsl" "tha-mili"];
-        };
-        "Cloud" = {
-          id = "rqeqw-kzojx";
-          path = "/home/${user}/Cloud";
-          devices = ["phone" "xtal" "tha-wsl"];
+        folders = filterAttrs (n: v: any (d: d == host) v.devices) {
+          "Extended Mind" = {
+            id = "tcgvs-gd77k";
+            path = "${homePath}/Extended Mind";
+            devices = ["phone" "xtal" "tha-wsl" "work-wsl"];
+          };
+          "Quick Share" = {
+            id = "mudfh-6fzjh";
+            path = "${homePath}/Quick Share";
+            devices = ["phone" "xtal" "tha-wsl" "work-wsl"];
+          };
+          ".config-win" = {
+            id = "wutmt-nb32y";
+            path = "/mnt/c/Users/" + config.my.windows.user + "/.config";
+            devices = ["phone" "tha-wsl" "work-wsl"];
+          };
+          "Books" = {
+            id = "ztshj-sh3ra";
+            path = "${homePath}/Books";
+            devices = ["phone" "xtal" "tha-wsl"];
+          };
+          "Camera" = {
+            id = "7pgoy-oxxrb";
+            path = "${homePath}/Camera";
+            devices = ["phone" "xtal" "tha-wsl"];
+          };
+          "Interlinked Cells" = {
+            id = "rfkva-mxs67";
+            path = "${homePath}/Interlinked Cells";
+            devices = ["phone" "xtal" "tha-wsl" "tha-mili"];
+          };
+          "Cloud" = {
+            id = "rqeqw-kzojx";
+            path = cloudPath;
+            devices = ["phone" "xtal" "tha-wsl"];
+          };
         };
       };
     };
-  };
-}
+
+    systemd.services.create-hardlinks = mkIf (hasAttr "Cloud" config.services.syncthing.settings.folders) {
+      wantedBy = ["multi-user.target"];
+      after = ["multi-user.target"];
+      script = ''
+        ln -f "${cloudPath}/FreeTube/history.db" "${homePath}/.config/FreeTube/history.db"
+        ln -f "${cloudPath}/FreeTube/playlists.db" "${homePath}/.config/FreeTube/playlists.db"
+        ln -f "${cloudPath}/FreeTube/profiles.db" "${homePath}/.config/FreeTube/profiles.db"
+        ln -f "${cloudPath}/FreeTube/settings.db" "${homePath}/.config/FreeTube/settings.db"
+      '';
+    };
+  }
