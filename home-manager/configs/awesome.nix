e9f58b70958c59c98ef55bd1e7ad575bf94b411a
grig-iv <abstractgrig@gmail.com>
2024-09-29 10:32:50 +0300

    cleanup awesomewm

--- home-manager/configs/awesome.nix
+++ home-manager/configs/awesome.nix
@@ -1,24 +1,9 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{...}: {
   xsession.windowManager.awesome.enable = true;
 
   my.shell.bookmarks.a = "~/.config/awesome";
 
-  services.sxhkd = {
-    enable = true;
-    # TODO: check why don't work
-    keybindings = {
-      "XF86Audio{Lower,Raise}Volume" = "${lib.getExe pkgs.pamixer} -{d,i} 5";
-      "XF86AudioMute" = "${lib.getExe pkgs.pamixer} -t";
-    };
-  };
-
   home = {
-    packages = [pkgs.sxhkd];
-
     shellAliases = {
       a = "cd $CONFIG/awesome & $EDITOR rc.lua";
     };
