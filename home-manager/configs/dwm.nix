aeea99799cee4ba42b0a597b525296c3f561ae0d
grig-iv <abstractgrig@gmail.com>
2024-10-06 21:24:37 +0300

    add gost

--- home-manager/configs/dwm.nix
+++ home-manager/configs/dwm.nix
@@ -4,34 +4,18 @@
   inputs,
   ...
 }: let
-  dwm-exe = lib.getExe inputs.grig-dwm.packages.${pkgs.system}.dwm;
+  dwm = lib.getExe inputs.grig-dwm.packages.${pkgs.system}.default;
+  gost = lib.getExe inputs.grig-gost.packages.${pkgs.system}.default;
   dwm-log = "$HOME/.local/share/dwm.log";
 in {
   xsession = {
     enable = true;
-    windowManager.command = "${dwm-exe} &> ${dwm-log}";
+    windowManager.command = "${dwm} &> ${dwm-log}";
     initExtra = ''
       rm ${dwm-log}
+      ${gost} &
     '';
   };
-
-  services.dwm-status = {
-    enable = true;
-    package = pkgs.dwm-status.override {enableNetwork = false;};
-    order = [
-      "audio"
-      "time"
-    ];
-    extraConfig = {
-      separator = " | ";
-      time.format = "%d %A %H:%M";
-      audio.template = " 󰕾 {VOL}%";
-    };
-  };
-
-  home.packages = with pkgs; [
-    alsa-utils
-  ];
 
   services.sxhkd.enable = true;
 }
