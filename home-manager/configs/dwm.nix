af2d8655bee6e03681ef5eb0a6b6e79e1db943ec
grig-iv <abstractgrig@gmail.com>
2024-10-10 22:38:02 +0300

    make dev-dwm test workflow

--- home-manager/configs/dwm.nix
+++ home-manager/configs/dwm.nix
@@ -7,10 +7,20 @@
   dwm = lib.getExe inputs.grig-dwm.packages.${pkgs.system}.default;
   gost = lib.getExe inputs.grig-gost.packages.${pkgs.system}.default;
   dwm-log = "$HOME/.local/share/dwm.log";
+  dwm-dev = "$HOME/sources/dwm/dwm";
+  dwm-dev-log = "$HOME/sources/dwm/dwm.log";
 in {
   xsession = {
     enable = true;
-    windowManager.command = "${dwm} &> ${dwm-log}";
+    windowManager.command = ''
+      ${dwm} &> ${dwm-log}
+
+      while [ -e ${dwm-dev} ]; do
+          ${dwm-dev} &> ${dwm-dev-log}
+          rm -f ${dwm-dev}
+          ${dwm} &> ${dwm-log}
+      done
+    '';
     initExtra = ''
       rm ${dwm-log}
       ${gost} &
