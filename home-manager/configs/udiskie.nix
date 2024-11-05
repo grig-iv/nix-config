b24fdfe4b91f1cdf6dc6ef81ccf03cab8c3cd752
grig-iv <abstractgrig@gmail.com>
2024-11-06 00:17:36 +0300

    cleanup

--- home-manager/configs/udiskie.nix
+++ home-manager/configs/udiskie.nix
@@ -1,13 +1,7 @@
-{config, ...}: {
+{...}: {
   # Reqired in nixos: services.udisks2.enable = true;
   services.udiskie = {
     enable = true;
     tray = "never";
-  };
-
-  my.shell.bookmarks = {
-    pa = "~/my-passport/Anime";
-    pm = "~/my-passport/Movies";
-    ps = "~/my-passport/Serials";
   };
 }
