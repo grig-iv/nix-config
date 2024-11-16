19fadf26b9efab9e55cd7ed6f9fc3adcca4039fc
grig-iv <abstractgrig@gmail.com>
2024-11-16 17:28:55 +0300

    completely disable redshift

--- home-manager/configs/redshift.nix
+++ home-manager/configs/redshift.nix
@@ -6,25 +6,25 @@
     longitude = 30.342980;
   };
 
-  xsession.initExtra = ''
-    systemctl --user stop redshift.service
-  '';
+  # xsession.initExtra = ''
+  #   systemctl --user stop redshift.service
+  # '';
 
-  systemd.user.services.start-redshift = {
-    Unit.Description = "start redshift";
-    Install.WantedBy = ["hm-graphical-session.target"];
-    Service.ExecStart = "systemctl --user start redshift.service";
-  };
-
-  systemd.user.timers.start-redshift = {
-    Unit.Description = "start redshift";
-    Install.WantedBy = ["timers.target"];
-    Timer = {
-      OnCalendar = [
-        "Sun,Mon..Thu *-*-* 22:30:00"
-        "Fri..Sat *-*-* 23:30:00"
-      ];
-      Unit = "start-redshift.service";
-    };
-  };
+  # systemd.user.services.start-redshift = {
+  #   Unit.Description = "start redshift";
+  #   Install.WantedBy = ["hm-graphical-session.target"];
+  #   Service.ExecStart = "systemctl --user start redshift.service";
+  # };
+  #
+  # systemd.user.timers.start-redshift = {
+  #   Unit.Description = "start redshift";
+  #   Install.WantedBy = ["timers.target"];
+  #   Timer = {
+  #     OnCalendar = [
+  #       "Sun,Mon..Thu *-*-* 22:30:00"
+  #       "Fri..Sat *-*-* 23:30:00"
+  #     ];
+  #     Unit = "start-redshift.service";
+  #   };
+  # };
 }
